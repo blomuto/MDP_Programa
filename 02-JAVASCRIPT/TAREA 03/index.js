e14fd28d45cebff92c4b4911a3087fcce9069609
blomuto <b.lomuto@gmail.com>
2023-07-28 16:03:30 -0300

    calculadora con Funciones Arrow

--- 02-JAVASCRIPT/TAREA 03/index.js
+++ 02-JAVASCRIPT/TAREA 03/index.js
@@ -20,19 +20,23 @@ const restar = (num1,num2) => {
 };
 
 const multiplicar = (num1) => {
+    let i, resultado;
     num1= parseInt(prompt("ingrese un numero para ver su tabla de multiplicar: "));
-    for (let i = 1; i <= 10; i++) {
-        const resultado = num1 * i;
+    for (i = 1; i <= 10; i++) {
+        resultado = num1 * i;
         console.log(`${num1} x ${i} = ${resultado}`);
     }
+    return (`${num1} x ${i} = ${resultado}`);
 };
 
 const dividir = (num1) => {
+    let i;
     num1= parseInt(prompt("ingrese un numero para ver su tabla de dividir: "));
     if (num1>0){
         for(i=1; i<=10; i++){
             console.log((num1*i), "/" ,num1, "=", i);
         }
+        return ((num1*i), "/" ,num1, "=", i);
     } else{
         console.log("Ingrese numero mayor a 1");
     }
